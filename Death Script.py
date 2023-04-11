6c89d99798b0ce7600bed9acbc529457f7a447c3
dotxdaemon <129806199+dotxdaemon@users.noreply.github.com>
2023-04-11 11:48:32 -0600

    Add files via upload

---
+++ Death Script.py
@@ -0,0 +1,8 @@
+age = float(input("What is your current age?\n"))
+death = int(input("At what age do you think you will die?\n"))
+
+days = round((death - age) * 365)
+weeks = round((death - age) * 52)
+month = round((death - age) * 12)
+
+print(f"You have {days} days, {weeks} weeks and {month} months left to live. What will you do?")
